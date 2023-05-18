53c4d810b99c92fa8b67b20afcff5ae41ed2768b
aionkenjiro <133928713+aionkenjiro@users.noreply.github.com>
2023-05-17 18:51:19 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showalert () {alert('¡hola, esta es una alerta desde javascript!');
+    }
+    document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})  
